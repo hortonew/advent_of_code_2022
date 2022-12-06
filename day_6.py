2be20db4828649b8786e77e288d76322dac8dd0c
Erik Horton <hortonew@gmail.com>
2022-12-06 10:46:43 -0700

    Day 6 cleanup

--- day_6.py
+++ day_6.py
@@ -4,15 +4,15 @@ from load import convert_to_list
 def get_total_including_marker(size: int) -> int:
     lines = convert_to_list("inputs/6.txt")
     total = 0
-    start_of_packet_marker: list[str] = []
+    marker: list[str] = []
     for character in lines[0]:
-        while character in start_of_packet_marker:
-            start_of_packet_marker.pop(0)
+        while character in marker:
+            marker.pop(0)
 
-        start_of_packet_marker.append(character)
+        marker.append(character)
         total += 1
 
-        if len(start_of_packet_marker) == size:
+        if len(marker) == size:
             break
 
     return total
